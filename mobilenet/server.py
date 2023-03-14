20b8be37840b4dc534aa3a4f21ea9fd21b5511ee
mhsong <mhsong@kookmin.ac.kr>
2023-03-14 23:40:28 +0900

    Fix typo

--- mobilenet/server.py
+++ mobilenet/server.py
@@ -39,6 +39,10 @@ def mobilenet_load_image(image_path):
         image_path,
         target_size=[224, 224])
 
+def inception_load_image(image_path):
+    return tf.keras.preprocessing.image.load_img(
+        image_path,
+        target_size=[299, 299])
 
 def image_to_array(image):
     return tf.keras.preprocessing.image.img_to_array(image, dtype=np.int32)
@@ -54,6 +58,22 @@ def save_model(model, saved_model_dir):
     shutil.rmtree(saved_model_dir, ignore_errors=True)
     model.save(saved_model_dir, include_optimizer=False, save_format='tf')
 
+mobilenetv1_image_path = './dataset/imagenet/imagenet_1000_raw/n02782093_1.JPEG'
+mobilenetv2_image_path = './dataset/imagenet/imagenet_1000_raw/n04404412_1.JPEG'
+inceptionv3_image_path = './dataset/imagenet/imagenet_1000_raw/n13040303_1.JPEG'
+
+mobilenetv1_test_image = mobilenet_load_image(mobilenetv1_image_path)
+mobilenetv2_test_image = mobilenet_load_image(mobilenetv2_image_path)
+inceptionv3_test_image = inception_load_image(inceptionv3_image_path)
+
+mobilenetv1_test_image_array = image_to_array(mobilenetv1_test_image)
+mobilenetv2_test_image_array = image_to_array(mobilenetv2_test_image)
+inceptionv3_test_image_array = image_to_array(inceptionv3_test_image)
+
+mobilenetv1_test_image_preprocessed = image_preprocess(mobilenetv1_test_image_array, 'mobilenet')
+mobilenetv2_test_image_preprocessed = image_preprocess(mobilenetv2_test_image_array, 'mobilenet_v2')
+inceptionv3_test_image_preprocessed = image_preprocess(inceptionv3_test_image_array, 'inception_v3')
+
 loaded_models = {}
 
 for model_name in models_to_load:
@@ -68,12 +88,6 @@ for model_name in models_to_load:
       print(f'model names must be in {model_names}')
       exit(1)
 
-mobilenetv1_image_path = './dataset/imagenet/imagenet_1000_raw/n02782093_1.JPEG'
-mobilenetv1_test_image = mobilenet_load_image(mobilenetv1_image_path)
-mobilenetv1_test_image_array = image_to_array(mobilenetv1_test_image)
-mobilenetv1_test_image_preprocessed = image_preprocess(mobilenetv1_test_image_array, 'mobilenet')
-
-
 print('saving and loading models completed!\n')
 
 app = Flask(__name__)
@@ -87,6 +101,30 @@ def mobilenetv1():
     inference_time = inference_end_time - inference_start_time
     
     return f'mobilenetv1 inference success\ninference time:{inference_time}\n'
+
+@app.route('/mobilenet_v2')
+def mobilenetv2():
+    inference_start_time = time.time()
+    result = loaded_models['mobilenet_v2'].predict(mobilenetv2_test_image_preprocessed)
+    inference_end_time = time.time()
+
+    inference_time = inference_end_time - inference_start_time
+
+    # print(result)
+
+    return f'mobilenetv2 inference success\ninference time:{inference_time}\n'
+
+@app.route('/inception_v3')
+def inceptionv3():
+    inference_start_time = time.time()
+    result = loaded_models['inception_v3'].predict(inceptionv3_test_image_preprocessed)
+    inference_end_time = time.time()
+
+    inference_time = inference_end_time - inference_start_time
+
+    # print(result)
+
+    return f'inceptionv3 inference success\ninference time:{inference_time}\n'
 
 @app.route('/healthcheck')
 def healthcheck():
